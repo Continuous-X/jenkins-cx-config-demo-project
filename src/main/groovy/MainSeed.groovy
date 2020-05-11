d8028fae2fe4587c0975dddd067d09d29ec10a24
Wolver Minion <mikeschmid71@googlemail.com>
2020-05-11 19:04:33 +0200

    test new master seed

--- src/main/groovy/MainSeed.groovy
+++ src/main/groovy/MainSeed.groovy
@@ -3,3 +3,28 @@ folder('Demo') {
     displayName('Demo')
     description('Demo Folder')
 }
+
+multiBranchJobs = [
+        [
+                name: 'jenkins-cx-shared-library',
+                url: 'https://github.com/Continuous-X/jenkins-cx-shared-library'
+        ]
+]
+
+multiBranchJobs.each { job ->
+    multibranchPipelineJob(job.name) {
+        displayName job.name
+        branchSources {
+            git {
+                id(job.name)
+                remote(job.url)
+                includes('JENKINS-*')
+            }
+        }
+        orphanedItemStrategy {
+            discardOldItems {
+                numToKeep(20)
+            }
+        }
+    }
+}
